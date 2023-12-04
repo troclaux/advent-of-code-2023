8f6aac562cdb0cc77b9447b3bb6caee00a289885
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-12-04 14:56:53 -0300

    finish day 1

--- day1/index.ts
+++ day1/index.ts
@@ -1,4 +1,31 @@
 import * as fs from 'fs';
-const lines: string[] = fs.readFileSync('input.txt', 'utf8').split('\n')
-console.log('test');
-console.log(lines);
+
+const lines: string[] = fs.readFileSync('input.txt', 'utf8').split('\n');
+
+function getFirstAndLastNumber(strings: string[]): number[] {
+    const filteredLines: string[] = new Array( lines.length ).fill('');
+    const output: number[] = [];
+
+    for (let idx = 0; idx < lines.length; idx++) {
+
+        for (let char = 0; char < lines[idx].length; char++) {
+
+            if ( !isNaN( Number( lines[idx][char] ) ) ) {
+                filteredLines[idx] = filteredLines[idx] + lines[idx][char];
+                break;
+            }
+        }
+
+        for (let char = lines[idx].length - 1; char >= 0; char--) {
+
+            if ( !isNaN( Number( lines[idx][char] ) ) ) {
+                filteredLines[idx] = filteredLines[idx] + lines[idx][char];
+                break;
+            }
+        }
+        output.push( Number( filteredLines[idx] ) );
+    }
+    return output;
+}
+
+console.log(getFirstAndLastNumber(lines));
