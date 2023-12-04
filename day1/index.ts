ac1ad8245115ec2f1efb2763c2c1e43142814fe3
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-12-04 16:07:31 -0300

    make program print all elements of the array

--- day1/index.ts
+++ day1/index.ts
@@ -1,25 +1,26 @@
 import * as fs from 'fs';
+const util = require('util')
 
 const lines: string[] = fs.readFileSync('input.txt', 'utf8').split('\n');
 
 function getFirstAndLastNumber(strings: string[]): number[] {
-    const filteredLines: string[] = new Array( lines.length ).fill('');
+    const filteredLines: string[] = new Array( strings.length ).fill('');
     const output: number[] = [];
 
-    for (let idx = 0; idx < lines.length; idx++) {
+    for (let idx = 0; idx < strings.length; idx++) {
 
-        for (let char = 0; char < lines[idx].length; char++) {
+        for (let char = 0; char < strings[idx].length; char++) {
 
-            if ( !isNaN( Number( lines[idx][char] ) ) ) {
-                filteredLines[idx] = filteredLines[idx] + lines[idx][char];
+            if ( !isNaN( Number( strings[idx][char] ) ) ) {
+                filteredLines[idx] = filteredLines[idx] + strings[idx][char];
                 break;
             }
         }
 
-        for (let char = lines[idx].length - 1; char >= 0; char--) {
+        for (let char = strings[idx].length - 1; char >= 0; char--) {
 
-            if ( !isNaN( Number( lines[idx][char] ) ) ) {
-                filteredLines[idx] = filteredLines[idx] + lines[idx][char];
+            if ( !isNaN( Number( strings[idx][char] ) ) ) {
+                filteredLines[idx] = filteredLines[idx] + strings[idx][char];
                 break;
             }
         }
@@ -28,4 +29,4 @@ function getFirstAndLastNumber(strings: string[]): number[] {
     return output;
 }
 
-console.log(getFirstAndLastNumber(lines));
+console.log( util.inspect( getFirstAndLastNumber(lines), { maxArrayLength: null } ) );
