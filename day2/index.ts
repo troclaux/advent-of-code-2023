351b82b4a68439c22fc0ca830baf47c0da2d9ba8
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-12-06 17:37:05 -0300

    complete part1 of day2

--- day2/index.ts
+++ day2/index.ts
@@ -3,21 +3,48 @@ import * as fs from 'fs';
 const input: string[] = fs.readFileSync('input1.txt', 'utf8').split('\n');
 input.pop();
 
-function sumOfIdsOfPossibleGames(games: string[]): number {
+let defaultBag: Map<string,number> = new Map([
+    [ 'red', 12 ],
+    [ 'green', 13 ],
+    [ 'blue', 14 ]
+]);
+
+function isGamePossible(gameStr: string, bag: Map<string, number>): boolean {
+    let gameSets: string[] = gameStr
+        .split(': ')[1]
+        .split('; ');
+    let singleColorCubes: string[] = [];
+    for (let idx = 0; idx < gameSets.length; idx++) {
+        singleColorCubes.push( ...gameSets[idx].split(', ') );
+    }
+
+    for (let idx = 0; idx < singleColorCubes.length; idx++) {
+        const [count, color] = singleColorCubes[idx].split(' ');
+        if (bag.get(color) && bag.get(color)! < Number(count)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function part1(games: string[], bag: Map<string, number>): number {
 
     let sum:number = 0;
 
-    let defaultBag: Map<string,number> = new Map([
-        [ 'red', 12 ],
-        [ 'green', 13 ],
-        [ 'blue', 14 ]
-    ]);
-
     for (let game of games) {
-        console.log(game);
-    }
 
+        if (isGamePossible(game, bag) ) {
+
+            let gameID: number = Number( 
+                game.split(':')[0]
+                .split(' ')[1]
+            );
+
+            sum += gameID;
+        }
+
+    }
     return sum;
 }
 
-sumOfIdsOfPossibleGames(input);
+part1(input, defaultBag);
