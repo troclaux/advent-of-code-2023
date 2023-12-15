5622fa349a78436f3ca4eaa91eb0204968ccba1d
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-12-15 12:00:11 -0300

    create variables to simplify addition of coordinat

--- day3/index.ts
+++ day3/index.ts
@@ -17,3 +17,14 @@ function addCoordinates(col: number, row: number, coordinate: number[]) {
     const result: [number, number] = [ coordinate[0] + col, coordinate[1] + row ];
     return result;
 }
+const coordinates: number[][] = [
+    [-1, -1], [0, -1], [1, -1],
+    [-1, 0], [1, 0],
+    [-1, 1], [0, 1], [1, 1]
+]
+
+const [
+    upLeft, up, upRight,
+    left, right,
+    downLeft, down, downRight
+] = coordinates;
