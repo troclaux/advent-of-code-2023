006632796494020bc4a99f1bb59e9fe248b88aee
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-12-15 11:56:24 -0300

    add isNumber function

--- day3/index.ts
+++ day3/index.ts
@@ -2,3 +2,7 @@ import * as fs from 'fs';
 
 const input: string[] = fs.readFileSync('input1.txt', 'utf8').split('\n');
 input.pop();
+
+function isNumber(char: string) {
+    return !isNaN(Number(char));
+}
