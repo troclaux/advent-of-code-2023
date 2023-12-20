2dbbd573dcb5818065c60fb79a0bb64aad353fc8
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-12-20 10:35:17 -0300

    fix parsing issue, ensure correct conversion of single-digit numbers

--- day4/index.ts
+++ day4/index.ts
@@ -3,21 +3,52 @@ import * as fs from 'fs';
 const input: string[] = fs.readFileSync('input1.txt', 'utf8').split('\n');
 input.pop();
 
+function getNumberCountMap(nums: number[]) {
+    let map = new Map<number,number>();
+    for (let num of nums) {
+        const count = map.get(num) || 0;
+        map.set(num, count + 1);
+    }
+    return map;
+}
+
+
 function part1(cards: string[]) {
     let sum: number = 0;
-    let list_of_winning_numbers: string[] = [];
-    let list_of_numbers: string[] = [];
+    let cardId: number = 1;
+    let cardPoints: number[] = [];
+
     for (let card of cards) {
-        const [curr_winning_numbers, curr_numbers] = card.split(':')[1].split('|');
-        console.log( { curr_winning_numbers } );
-        console.log( { curr_numbers } );
-        // get both lists as strings
-        // separate the two lists
-        // separate the numbers
-        // add numbers to hashmap
-        // list_of_winning_numbers.push(
+
+        let matchCount: number = 0;
+        const [winningNumbersStr, numbersStr] = card.split(':')[1].split('|');
+
+        let winningNumbers: number[] = winningNumbersStr
+            .split(' ')
+            .map(numStr => parseInt( numStr, 10 ))
+            .filter( numStr => { return !Number.isNaN(numStr) } );
+
+        let numbers: number[] = numbersStr
+            .split(' ')
+            .map(numStr => parseInt( numStr, 10 ))
+            .filter( numStr => { return !Number.isNaN(numStr) } );
+
+        let countMap: Map<number, number> = getNumberCountMap(numbers);
+        let matches: number[] = [];
+
+        for ( let winningNumber of winningNumbers ) {
+            if ( countMap.get( winningNumber ) ) {
+                matches.push(winningNumber);
+                matchCount++;
+            }
+        }
+
+        console.log( cardId, winningNumbers, numbers, matches );
+        cardPoints.push(matchCount);
+        cardId++;
     }
-    return sum;
+    
+    return cardPoints;
 }
 
 // console.dir(partNumbers, {'maxArrayLength': null});
